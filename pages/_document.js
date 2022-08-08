d6d0acfbaef2abcb795822470e587478d3e67da7
Andrew Borstein <andrew.borstein@lightspeedhq.com>
2022-08-08 16:48:21 -0700

    Import remote MFE on production
    The @topbar MFE bundle was manually copy-pasted to mocky.io. To run it locally clone https://github.com/lightspeed-hospitality/single-spa-top-bar locally, check out the systemjs branch, and start the dev server with `yarn start`.

--- pages/_document.js
+++ pages/_document.js
@@ -14,12 +14,26 @@ export default class MyDocument extends Document {
                 "imports": {
                   "react": "https://cdn.jsdelivr.net/npm/react@17/umd/react.production.min.js",
                   "react-dom": "https://cdn.jsdelivr.net/npm/react-dom@17/umd/react-dom.production.min.js",
-                  "@topbar": "http://localhost:9010/lightspeed-hospitality-top-bar.js"
+                  "@topbar": "https://run.mocky.io/v3/13c6d07a-3f35-4614-9f6a-a97a8af24a98"
                 }
               }
             `,
             }}
           />
+          {process.env.NODE_ENV === 'development' && (
+            <script
+              type="systemjs-importmap"
+              dangerouslySetInnerHTML={{
+                __html: `
+                {
+                  "imports": {
+                    "@topbar": "http://localhost:9010/lightspeed-hospitality-top-bar.js"
+                  }
+                }
+              `,
+              }}
+            />
+          )}
           <script src="https://cdn.jsdelivr.net/npm/systemjs@6/dist/system.min.js" />
           <script src="https://cdn.jsdelivr.net/npm/systemjs@6/dist/extras/amd.min.js" />
         </Head>
